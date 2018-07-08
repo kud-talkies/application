0ba0c54d4f46cb16b04eda051a2634a3276dc1da
chandrashekar <chandrashekar@192.168.200.203>
2018-07-08 01:06:09 -0400

    setup

---
+++ kud-application/src/test/java/com/kmukhtalkies/application/KudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kmukhtalkies.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
